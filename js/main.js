d392482d0b302ccf14831f9ebf3c501b23ad2216
Stefan Guggisberg <stefan@bassplayer.ch>
2017-11-01 10:42:29 +0100

    new start

---
+++ js/main.js
@@ -0,0 +1,28 @@
+var hamburgerIcon = document.getElementById('hamburgerIcon');
+var sideBar = document.querySelector('nav ul');
+var navLinks = document.querySelectorAll('#navBar ul li');
+
+var openMenu = function () {
+    hamburgerIcon.classList.toggle('open');
+    sideBar.classList.toggle('sideBarOpen');
+};
+
+hamburgerIcon.addEventListener('click', openMenu);
+for (var i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener('click', function () {
+        sideBar.classList.toggle('sideBarOpen');
+        hamburgerIcon.classList.toggle('open');
+
+
+    });
+}
+
+
+// document.addEventListener('scroll', function () {
+//     var news = document.getElementById('news');
+//     console.log(window.scrollY, news.offsetTop);
+//     console.log('')
+//     if (window.scrollY === news.offsetTop) {
+//         alert('asda');
+//     }
+// });
