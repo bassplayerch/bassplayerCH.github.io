4f11047ffc22ff4f54471aadfaed1b63f1b8c9e7
Stefan Guggisberg <stefan@bassplayer.ch>
2017-11-26 19:56:00 +0100

    asd

--- js/main.js
+++ js/main.js
@@ -1,28 +1,41 @@
-var hamburgerIcon = document.getElementById('hamburgerIcon');
-var sideBar = document.querySelector('nav ul');
-var navLinks = document.querySelectorAll('#navBar ul li');
-
-var openMenu = function () {
-    hamburgerIcon.classList.toggle('open');
-    sideBar.classList.toggle('sideBarOpen');
-};
-
-hamburgerIcon.addEventListener('click', openMenu);
-for (var i = 0; i < navLinks.length; i++) {
-    navLinks[i].addEventListener('click', function () {
-        sideBar.classList.toggle('sideBarOpen');
-        hamburgerIcon.classList.toggle('open');
-
-
+$(document).ready(function () {
+    console.log('loaded');
+    $('a[href^=\\#]').on('click', function(e){
+        console.log('clicked');
+        var href = $(this).attr('href');
+        console.log(href);
+        $('html, body').animate({
+            scrollTop:$(href).offset().top
+        },'slow');
+        e.preventDefault();
     });
-}
+    var hamburgerIcon = document.getElementById('hamburgerIcon');
+    var sideBar = document.querySelector('.menuLinks');
+    var navLinks = document.querySelectorAll('.menuLinks a');
+    var menuLinks = document.querySelectorAll('.closeMenu');
 
 
-// document.addEventListener('scroll', function () {
-//     var news = document.getElementById('news');
-//     console.log(window.scrollY, news.offsetTop);
-//     console.log('')
-//     if (window.scrollY === news.offsetTop) {
-//         alert('asda');
-//     }
-// });
+    var openMenu = function () {
+        hamburgerIcon.classList.toggle('open');
+        var sideBarStyle = sideBar.style.display;
+        sideBarStyle == "flex" ? sideBar.style.display = "none" : sideBar.style.display = "flex";
+    };
+
+    hamburgerIcon.addEventListener('click', openMenu);
+
+
+
+
+    // for (var i = 0; i < navLinks.length; i++) {
+    //     navLinks[i].addEventListener('click', function () {
+    //         hamburgerIcon.classList.toggle('open');
+    //     });
+    // }
+
+    for (var i = 0; i < menuLinks.length; i++) {
+        menuLinks[i].addEventListener('click', function () {
+            sideBar.style.display = "none";
+            hamburgerIcon.classList.toggle('open');
+        });
+    }
+});
